68f0ade806c7f7002e2cd6201e80df2a53686814
MD. SHAHRIAR AFRIDI <Shahriarafridi66@gmail.com>
2025-05-02 22:01:45 +0600

    initial commit

---
+++ src/main.py
@@ -0,0 +1,25 @@
+# app.py
+import streamlit as st
+from recommend import df, recommend_songs
+
+# Set custom Streamlit page config
+st.set_page_config(
+    page_title="Music Recommender 🎵",
+    page_icon="🎧",  # You can also use a path to a .ico or .png file
+    layout="centered"
+)
+
+
+st.title("🎶 Hollywoord Song Recommender")
+
+song_list = sorted(df['song'].dropna().unique())
+selected_song = st.selectbox("🎵 Select a song:", song_list)
+
+if st.button("🚀 Recommend Similar Songs"):
+    with st.spinner("Finding similar songs..."):
+        recommendations = recommend_songs(selected_song)
+        if recommendations is None:
+            st.warning("Sorry, song not found.")
+        else:
+            st.success("Top similar songs:")
+            st.table(recommendations)
